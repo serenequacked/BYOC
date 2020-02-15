b1aabcaaecb1fcdd67a17121038ddb61eae9e0bb
ruyin <ruyinliang@Ruyins-MacBook-Pro.local>
2020-02-15 20:43:05 +0800

    qr code implementation

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scannerView = (ZXingScannerView)findViewById(R.id.zxscan);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        scannerView.setResultHandler(MainActivity.this);
+                        scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "You must accept this permission", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+
+    @Override
+    protected void onDestroy() {
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        //txtResult.setText(rawResult.getText());
+        //scannerView.startCamera();
+        Intent i = new Intent(getApplicationContext(), NextActivity.class);
+        startActivity(i);
+    }
+}
